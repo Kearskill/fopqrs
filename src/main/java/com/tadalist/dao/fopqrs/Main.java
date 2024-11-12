cd367d383e738590380184eb877f60782b48f9a7
Kearskill <kearskill@gmail.com>
2024-11-13 03:49:08 +0800

    first commit dao dao dao

--- src/main/java/com/tadalist/dao/fopqrs/Main.java
+++ src/main/java/com/tadalist/dao/fopqrs/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Users Leith = new Users(10000, "leith@gmail.com", "leith", "password");
         System.out.print(Leith);
-
     }
 }
