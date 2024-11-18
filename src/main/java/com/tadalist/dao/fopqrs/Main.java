d195838f979cdb631a03633ce876d7cd3e70bf51
Kearskill <kearskill@gmail.com>
2024-11-18 14:19:05 +0800

    letak sikit comment

--- src/main/java/com/tadalist/dao/fopqrs/Main.java
+++ src/main/java/com/tadalist/dao/fopqrs/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Users Leith = new Users(10000, "leith@gmail.com", "leith", "password");
         System.out.print(Leith);
+
+//        CONTOH INSERT
+//        INSERT INTO `tadalist_db`.`users` (`UserID`, `Email`, `UserName`, `UserPassword`, `NotificationPreference`, `LastLogin`) VALUES ('10001', 'leith@gmail.com', 'Leith', 'password', 'EMAIL', '2024-11-18 2:14:20');
+
+//        CONTOH UPDATE
+//        UPDATE `tadalist_db`.`users` SET `UserName` = 'Leithi' WHERE (`UserID` = '10001');
+
+//        CONTOH DELETE
+//        DELETE FROM `tadalist_db`.`users` WHERE (`UserID` = '10002');
     }
 }
