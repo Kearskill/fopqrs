57f3cdc139952966bc20509714bef410d0f2e708
AZAI1007 <ameerzafran05@gmail.com>
2024-11-26 01:11:04 +0800

    settle connection

--- src/main/java/com/tadalist/dao/fopqrs/Main.java
+++ src/main/java/com/tadalist/dao/fopqrs/Main.java
@@ -1,7 +1,12 @@
 package com.tadalist.dao.fopqrs;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+        try {
         Users Leith = new Users(10000, "leith@gmail.com", "leith", "password");
         System.out.print(Leith);
 
@@ -13,5 +18,22 @@ public class Main {
 
 //        CONTOH DELETE
 //        DELETE FROM `tadalist_db`.`users` WHERE (`UserID` = '10002');
+        // Load MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Connect to Database
+        String url = "jdbc:mysql://localhost:3306/abd"; //change abd to whichever schema youre using
+        String user = "root";
+        String password = "localroot";
+        System.out.println("Connecting to: " + url + " with user " + user);
+        Connection con = DriverManager.getConnection(url, user, password);
+
+        }catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQL Exception occurred:");
+            e.printStackTrace();
+        }
     }
 }
